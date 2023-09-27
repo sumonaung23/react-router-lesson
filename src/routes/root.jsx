d0f741176a53daddb584c5944a7910d4c8e638fc
sumonaung23 <sumonaung575@gmail.com>
2023-09-27 23:47:50 +0630

    added getContacts and loader async function

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,10 @@
     import { Outlet, Link } from "react-router-dom";
+    import { getContacts } from "../contacts";
+
+    export async function loader() {
+        const contacts = await getContacts();
+        return { contacts };
+    }
     
     export default function Root() {
         return (
