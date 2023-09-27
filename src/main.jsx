4f72712b169f7cfa7af3ee9708fabc5c1eca9aca
sumonaung23 <sumonaung575@gmail.com>
2023-09-27 23:48:54 +0630

    configured the loader on the route

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,14 @@ import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
