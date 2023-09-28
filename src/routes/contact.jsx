a0abd5ecd5c07020a0f2c94c9fb9129b60ab974d
sumonaung23 <sumonaung575@gmail.com>
2023-09-28 23:31:36 +0630

    added a loader to the contact page and access data with useLoaderData

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,6 +1,13 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+
+export async function loader({ params }) {
+    const contact = await getContact(params.contactId);
+    return { contact };
+}
 
 export default function Contact() {
+    //const { contact } = useLoaderData();
     const contact = {
         first: "Your",
         last: "Name",
@@ -12,59 +19,59 @@ export default function Contact() {
 
     return (
         <div id="contact">
-        <div>
-            <img
-            key={contact.avatar}
-            src={contact.avatar || null}
-            />
-        </div>
-
-        <div>
-            <h1>
-            {contact.first || contact.last ? (
-                <>
-                {contact.first} {contact.last}
-                </>
-            ) : (
-                <i>No Name</i>
-            )}{" "}
-            <Favorite contact={contact} />
-            </h1>
-
-            {contact.twitter && (
-            <p>
-                <a
-                target="_blank"
-                href={`https://twitter.com/${contact.twitter}`}
-                >
-                {contact.twitter}
-                </a>
-            </p>
-            )}
-
-            {contact.notes && <p>{contact.notes}</p>}
+            <div>
+                <img
+                key={contact.avatar}
+                src={contact.avatar || null}
+                />
+            </div>
 
             <div>
-            <Form action="edit">
-                <button type="submit">Edit</button>
-            </Form>
-            <Form
-                method="post"
-                action="destroy"
-                onSubmit={(event) => {
-                if (
-                    !confirm(
-                    "Please confirm you want to delete this record."
-                    )
-                ) {
-                    event.preventDefault();
-                }
-                }}
-            >
-                <button type="submit">Delete</button>
-            </Form>
+                <h1>
+                {contact.first || contact.last ? (
+                    <>
+                    {contact.first} {contact.last}
+                    </>
+                ) : (
+                    <i>No Name</i>
+                )}{" "}
+                <Favorite contact={contact} />
+                </h1>
+
+                {contact.twitter && (
+                <p>
+                    <a
+                    target="_blank"
+                    href={`https://twitter.com/${contact.twitter}`}
+                    >
+                    {contact.twitter}
+                    </a>
+                </p>
+                )}
+
+                {contact.notes && <p>{contact.notes}</p>}
+
+                <div>
+                    <Form action="edit">
+                        <button type="submit">Edit</button>
+                    </Form>
+                    <Form
+                        method="post"
+                        action="destroy"
+                        onSubmit={(event) => {
+                        if (
+                            !confirm(
+                            "Please confirm you want to delete this record."
+                            )
+                        ) {
+                            event.preventDefault();
+                        }
+                        }}
+                    >
+                        <button type="submit">Delete</button>
+                    </Form>
+                </div>
             </div>
-        </div>
         </div>
     );
 }
